8b7ee66000b5f2113d48856226a65f9cf4acb5a8
asdfasdf <asdfasdf@qawer.com>
2025-12-04 08:37:19 +0100

    .

--- day04_2.py
+++ day04_2.py
@@ -14,22 +14,45 @@ for r in range(len(data)):
 temp = len(papers)
 while True:
     remove = set()
-    for r in range(len(data)):
-        for c in range(len(data[0])):
-            p = 0
+    for r,c in papers:
+        p = 0
 
-            if (r,c) in papers:
-                for dr, dc in dirs:
-                    if r+dr >= len(data) or r+dr < 0 or c+dc >= len(data[0]) or c+dc < 0: continue
-                    if (r+dr, c+dc) in papers:
-                        p +=1
+        if (r,c) in papers:
+            for dr, dc in dirs:
+                if r+dr >= len(data) or r+dr < 0 or c+dc >= len(data[0]) or c+dc < 0: continue
+                if (r+dr, c+dc) in papers:
+                    p +=1
 
-                if p <4:
-                    remove.add((r,c))
-                    tot += 1
+            if p <4:
+                remove.add((r,c))
+                tot += 1
     
     if len(remove) == 0:
         print(temp-len(papers))
         break
 
     papers = papers - remove
+
+
+
+    # while True:
+    # remove = set()
+    # for r in range(len(data)):
+    #     for c in range(len(data[0])):
+    #         p = 0
+
+    #         if (r,c) in papers:
+    #             for dr, dc in dirs:
+    #                 if r+dr >= len(data) or r+dr < 0 or c+dc >= len(data[0]) or c+dc < 0: continue
+    #                 if (r+dr, c+dc) in papers:
+    #                     p +=1
+
+    #             if p <4:
+    #                 remove.add((r,c))
+    #                 tot += 1
+    
+    # if len(remove) == 0:
+    #     print(temp-len(papers))
+    #     break
+
+    # papers = papers - remove
